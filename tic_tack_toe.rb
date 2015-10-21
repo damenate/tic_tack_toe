9c80dee8afd4dc232d7825c648bdee626c80a31f
Da-Me <damekim1206@gmail.com>
2015-10-21 17:39:40 -0400

    add methods for x and y but no logic

--- tic_tack_toe.rb
+++ tic_tack_toe.rb
@@ -20,3 +20,13 @@ def display_board
   array = [[" ", " ", " "],
 
  ]]
+
+
+
+ winning_combos = [
+   # Horizontal wins:
+   ["a1", "a2", "a3"], ["b1", "b2", "b3"], ["c1", "c2", "c3"],
+   # Vertical wins:
+   ["a1", "b1", "c1"], ["a2", "b2", "c2"], ["a3", "b3", "c3"],
+   # Diagonal wins:
+   ["a1", "b2", "c3"], ["a3", "b2", "c1"]]
--- ticky_tacky.rb
+++ ticky_tacky.rb
@@ -29,7 +29,7 @@
   end
 
   def x_of
-    
+
   end
 
   def y_of
